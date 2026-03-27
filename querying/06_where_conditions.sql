17bb2ca4c97e870aa820f8aa44b9b819e805e147
WhitneyLB <whitneyberghuis@gmail.com>
2026-03-27 14:04:43 -0700

    Update 06_where_conditions.sql

--- querying/06_where_conditions.sql
+++ querying/06_where_conditions.sql
@@ -1,3 +1,2 @@
-SELECT title
-    FROM episodes
-        WHERE season = 6 AND air_date BETWEEN '2007-01-01' AND '2007-12-31';
+SELECT title FROM episodes
+WHERE season = 6 AND air_date BETWEEN '2007-01-01' AND '2007-12-31';
